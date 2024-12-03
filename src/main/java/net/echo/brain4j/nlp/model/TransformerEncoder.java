accf2b19ab9ef2dd421a7a284b80fa0a752332b0
xEcho1337 <xreflection@outlook.it>
2024-12-03 19:25:22 +0100

    Fixed multi layer perceptron after attention

--- src/main/java/net/echo/brain4j/nlp/model/TransformerEncoder.java
+++ src/main/java/net/echo/brain4j/nlp/model/TransformerEncoder.java
@@ -1,7 +1,6 @@
 package net.echo.brain4j.nlp.model;
 
 import net.echo.brain4j.activation.Activations;
-import net.echo.brain4j.layer.Layer;
 import net.echo.brain4j.layer.impl.DenseLayer;
 import net.echo.brain4j.layer.impl.LayerNorm;
 import net.echo.brain4j.model.Model;
@@ -13,7 +12,7 @@ import java.util.List;
 
 public class TransformerEncoder extends Model {
 
-    private List<Model> denseLayers;
+    private Model feedForward;
     private LayerNorm normalizer;
     private MultiHeadAttention attention;
     private int contextSize;
@@ -22,47 +21,24 @@ public class TransformerEncoder extends Model {
         this.normalizer = new LayerNorm();
         this.attention = new MultiHeadAttention(numHeads, contextSize, dimension, temperature);
         this.contextSize = contextSize;
-        this.denseLayers = new ArrayList<>();
-
-        for (int i = 0; i < contextSize; i++) {
-            Model model = new Model(
-                    new DenseLayer(dimension, Activations.LINEAR),
-                    new DenseLayer(1024, Activations.RELU),
-                    new DenseLayer(contextSize, Activations.LINEAR)
-            );
-
-            this.denseLayers.add(model);
-        }
-
-        /*for (int i = 0; i < layers; i++) {
-            add(
-                    new LayerNorm(),
-                    new MultiHeadAttention(numHeads, contextSize, dimension, temperature),
-                    // Add
-                    new LayerNorm(),
-                    new DenseLayer(1024, Activations.RELU),
-                    new DenseLayer(contextSize, Activations.LINEAR)
-                    // Add (again)
-            );
-        }
-
-        add(new LayerNorm());*/
+        this.feedForward = new Model(
+                new DenseLayer(dimension, Activations.LINEAR),
+                new DenseLayer(4 * dimension, Activations.RELU),
+                new DenseLayer(dimension, Activations.LINEAR)
+        );
     }
 
     public List<Vector> transform(List<Vector> embeddings) {
         List<Vector> resulting = new ArrayList<>();
 
-        for (int i = 0; i < embeddings.size(); i++) {
-            Vector embedding = Vector.of(embeddings.get(i).toArray());
-
+        for (Vector vector : embeddings) {
+            Vector embedding = Vector.of(vector.toArray());
             normalizer.normalize(embedding);
 
             Vector attended = attention.attend(embedding.toArray());
-
             normalizer.normalize(attended);
 
-            Vector result = denseLayers.get(i).predict(attended);
-
+            Vector result = feedForward.predict(attended);
             normalizer.normalize(result);
 
             resulting.add(result);
